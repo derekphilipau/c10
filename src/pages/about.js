91307c52cf93da61addafe0e030534973980bce1
Derek Au <derek@glazy.org>
2018-07-08 23:53:19 -0400

    more updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,22 +3,26 @@ import Link from "gatsby-link";
 import cautiousDog from "../images/cautious-dog.svg";
 
 const AboutPage = () => (
-  <div className="flex flex-col md:flex-row items-center">
-    <div className="md:w-2/3 md:mr-8">
-      <p className="border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
+  <div className="flex flex-wrap flex-row items-start">
+    <div className="w-full md:w-1/2 lg:w-1/2 px-4">
+      <div className="h-64 w-full rounded shadow bg-grey p-3 mb-4">b</div>
+      <div className="h-32 w-full rounded p-3 mb-4">
         The point is... to live one's life in the full complexity of what one
         is, which is something much darker, more contradictory, more of a
         maelstrom of impulses and passions, of cruelty, ecstacy, and madness,
         than is apparent to the civilized being who glides on the surface and
         fits smoothly into the world.
-      </p>
-      <p className="font-bold mt-4 text-right text-xs uppercase">
-        — Thomas Nagel
-      </p>
+      </div>
     </div>
-
-    <div className="w-2/3 md:w-1/3">
-      <img src={cautiousDog} />
+    <div className="w-full md:w-1/2 lg:w-1/2 px-4">
+      <div className="h-64 w-full rounded shadow bg-grey p-3 mb-4"></div>
+      <div className="h-32 w-full rounded p-3 mb-4">
+        The point is... to live one's life in the full complexity of what one
+        is, which is something much darker, more contradictory, more of a
+        maelstrom of impulses and passions, of cruelty, ecstacy, and madness,
+        than is apparent to the civilized being who glides on the surface and
+        fits smoothly into the world.
+      </div>
     </div>
   </div>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import Link from "gatsby-link";
 
 const IndexPage = () => (
-    <div className="text-center">
+    <div className="flex-1 w-100">
 
-      <div className="flex flex-wrap flex-row items-start">
+      <div className="h-64 bg-black mb-8 pt-24 text-center">
+        <span className="text-5xl">Code is craft.</span>
+      </div>
+
+      <div className="flex flex-wrap flex-row items-start px-4">
         <div className="w-full md:w-1/2 lg:w-1/2 px-4">
           <div className="h-32 w-full rounded shadow bg-grey p-3 mb-4">b</div>
           <div className="h-64 w-full rounded shadow bg-grey p-3 mb-4">a</div>
@@ -15,10 +19,7 @@ const IndexPage = () => (
         </div>
       </div>
 
-    <h1 className="inline-block my-8 p-3">
-      C10 Consulting
-    </h1>
-    <p className="leading-loose">
+    <p className="leading-loose mx-8 py-24">
       This is a barebones starter for Gatsby styled using{" "}
       <a
         href="https://tailwindcss.com/"
@@ -28,11 +29,6 @@ const IndexPage = () => (
       </a>, a utility-first CSS framework.
     </p>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 );
 
