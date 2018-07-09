a992e1c601e9ad000eca0684096ef4d9221d5df7
Derek Au <derek@glazy.org>
2018-07-08 21:41:26 -0400

    minor change

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -39,6 +39,15 @@ const Footer = () => (
             <iframe className="w-full h-full" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d11812.443474393653!2d-77.7975740037964!3d42.254804877099694!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1531099586659" width="400" height="300" frameborder="0" allowfullscreen></iframe>
           </div>
         </p>
+      </div>
+      <div className="w-full md:w-1/3 px-4 mb-8">
+        <h3 className="uppercase mb-4 pb-4 border-b">
+          About Us
+        </h3>
+        <p className="py-2">
+          Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae ornare massa, at consequat ipsum. Sed ullamcorper, dui consequat vehicula volutpat dolor.
+        </p>
         <h3 className="uppercase mb-4 py-4 border-b">
           Socialize
         </h3>
@@ -53,15 +62,6 @@ const Footer = () => (
             <svg className="fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6.29 18.25c7.55 0 11.67-6.25 11.67-11.67v-.53c.8-.59 1.49-1.3 2.04-2.13-.75.33-1.54.55-2.36.65a4.12 4.12 0 0 0 1.8-2.27c-.8.48-1.68.81-2.6 1a4.1 4.1 0 0 0-7 3.74 11.65 11.65 0 0 1-8.45-4.3 4.1 4.1 0 0 0 1.27 5.49C2.01 8.2 1.37 8.03.8 7.7v.05a4.1 4.1 0 0 0 3.3 4.03 4.1 4.1 0 0 1-1.86.07 4.1 4.1 0 0 0 3.83 2.85A8.23 8.23 0 0 1 0 16.4a11.62 11.62 0 0 0 6.29 1.84"></path></svg>
             <span className="ml-4">Twitter</span>
           </a>
-        </p>
-      </div>
-      <div className="w-full md:w-1/3 px-4 mb-8">
-        <h3 className="uppercase mb-4 pb-4 border-b">
-          About Us
-        </h3>
-        <p className="py-2">
-          Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae ornare massa, at consequat ipsum. Sed ullamcorper, dui consequat vehicula volutpat dolor.
         </p>
       </div>
     </div>
